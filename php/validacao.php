7779a4e8d03f61a3bfac709b9d0f76ddc7c54955
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-03-19 08:57:07 -0300

    Função que analisa os dois últimos dígitos do CPF

--- php/validacao.php
+++ php/validacao.php
@@ -3,7 +3,8 @@
   
   // 1. VALIDAÇÃO
   /*
-    ABCDEFGHI/JK
+    CPF hipotético: ABCDEFGHI-JK
+    J e K são os "Últimos dígitos do CPF"
     O resultado da soma, 10A + 9B + 8C + 7D + 6E + 5F + 4G + 3H + 2I, é dividido por 11.
   */
   function validandoEntrada($entCpf)
@@ -18,7 +19,49 @@
     return $cpfRetorno;
   }
 
+  function calculaUltimosDigitosCpf($entCpf, $opcao)
+  {
+    // Declaração de variáveis e escolha de J ou K
+    if ($opcao == 0) {
+      $cteOperacaoDiferenca = 10;
+      $limiteLoopMultiplicacao = 9;
+    } elseif ($opcao == 1) {
+      $cteOperacaoDiferenca = 11;
+      $limiteLoopMultiplicacao = 10;
+    }
+    $digitoRetorno = 0;
+    $cteOnze = 11;
+    $arrayCpf = str_split($entCpf);
+    $arrayCpfMultiplicado = [];
+    $somaDigitosCpf = 0;
+    $restoDivisao = 0;
+    $condicaoResto = false;
+    
+    // Multiplicação e soma dos dígitos
+    for ($i=0; $i < $limiteLoopMultiplicacao; $i++) { 
+      // Multiplicando pelas diferenças de 10.
+      $arrayCpfMultiplicado[$i] = ($cteOperacaoDiferenca - $i) * $arrayCpf[$i];
+    }
+    $somaDigitosCpf = array_sum($arrayCpfMultiplicado);
+    
+    // Analisando o resto da divisão por onze e retornando o resultado
+    $restoDivisao = $somaDigitosCpf % $cteOnze;
+    $condicaoResto = $restoDivisao == 0 || $restoDivisao == 1;
+    if ($condicaoResto) {
+      return $digitoRetorno;
+    } elseif ($restoDivisao >= 2 && $restoDivisao <= 10) {
+      return $cteOnze - $restoDivisao;
+    }
+  }
+
   $cpfResultado = validandoEntrada($entradaCpf);
-  $jsonCpfResultado = [$cpfResultado];
-  echo json_encode($jsonCpfResultado);
+  $arrayUltimosDigitosCpf = [];
+  for ($i=0; $i <= 1; $i++) { 
+    // Se $i for 0, então calcula-se J, senão, calcula-se K.
+    $arrayUltimosDigitosCpf[$i] = calculaUltimosDigitosCpf($cpfResultado, $i);
+
+    // FALTA COMPARAR COM OS DOIS ÚLTIMOS DÍGITOS REAIS
+  }
+
+  echo json_encode($arrayUltimosDigitosCpf);
 ?>
