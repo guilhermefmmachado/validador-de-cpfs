24585065d00bbf9be675fce5a5cb2951eefbeb21
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-04-11 10:46:04 -0300

    Criação da classe Cpf

---
+++ php/Cpf.php
@@ -0,0 +1,11 @@
+<?php
+  class Cpf{
+    $cpfRecebido = "";
+    private $cpfFormatado = "";
+    $isValido;
+
+    function __construct($cpfRecebido){
+      $resultado = preg_replace("/\.|-/", "", $cpfRecebido; // Trocar Esquema de URLs e validação
+    }
+  }
+?>
