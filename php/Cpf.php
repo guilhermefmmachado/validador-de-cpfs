672ad8f8a3dcf463df3a49121b4250a415d7d824
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-04-12 10:53:13 -0300

    Classe Cpf

--- php/Cpf.php
+++ php/Cpf.php
@@ -1,11 +1,71 @@
 <?php
+  // Considere "p" -> Parâmetro
+  // Considere xxx.xxx.xxx-yy um número hipotético de cpf para a leitura
+
   class Cpf{
-    $cpfRecebido = "";
-    private $cpfFormatado = "";
-    $isValido;
+    public $cpfRecebido;
+    private $cpfFormatado;
+    public $isValido;
 
     function __construct($cpfRecebido){
-      $resultado = preg_replace("/\.|-/", "", $cpfRecebido; // Trocar Esquema de URLs e validação
+      // Trocar Esquema de URLs e validação
+      $this->cpfFormatado = $this->formatarCpf($cpfRecebido);
+    }
+
+    private function formatarCpf($pCpfRecebido) {
+      $resultado = preg_replace("/\.|-/", "", $pCpfRecebido);
+      if (strLen($resultado) != 11) {
+        throw new Exception("O número recebido não é válido. Um cpf deve conter 11 dígitos numéricos.");
+      }
+      return $resultado;
+    }
+
+    public function validarCpf() {
+      $resultado;
+      $novePrimeirosDigitos = str_split($this->cpfFormatado);
+      array_pop($novePrimeirosDigitos);
+      array_pop($novePrimeirosDigitos);
+      $dezPrimeirosDigitos = array_push(
+        $novePrimeirosDigitos, $this->calcularPrimeiroDigitoY($novePrimeirosDigitos)
+      );
+      $cpfValidacao = array_push(
+        $dezPrimeirosDigitos, $this->calcularSegundoDigitoY($dezPrimeirosDigitos)
+      );
+      $cpfValidacao = join($cpfValidacao);
+      $resultado = $cpfValidacao == $this->cpfFormatado ? "Cpf válido" : "Cpf inválido";
+    }
+
+    private function calcularPrimeiroDigitoY($pNovePrimeirosDigitos) {
+      $multiplicador = 10;
+      $listaNoveDigitosMultiplicados = [];
+      $restoDivisao11 = 0;
+
+      for ($i=0; $i < count($pNovePrimeirosDigitos) - 1; $i++) { 
+        $listaNoveDigitosMultiplicados[$i] = $pNovePrimeirosDigitos[$i] * $multiplicador;
+        $multiplicador--;
+      }
+
+      $restoDivisao11 = array_sum($listaNoveDigitosMultiplicados) % 11;
+      $condicaoRetorno = $restoDivisao11 != 0 || $restoDivisao11 != 1;
+      if ($condicaoRetorno) { return 11 - $restoDivisao11; }
+      return 0;
+      // Retornando inteiro em array de string!
+    }
+
+    private function calcularSegundoDigitoY($pDezPrimeirosDigitos) {
+      $multiplicador = 11;
+      $listaDezDigitosMultiplicados = [];
+      $restoDivisao11 = 0;
+
+      for ($i=0; $i < count($pDezPrimeirosDigitos) - 1; $i++) { 
+        $listaDezDigitosMultiplicados[$i] = $pDezPrimeirosDigitos[$i] * $multiplicador;
+        $multiplicador--;
+      }
+
+      $restoDivisao11 = array_sum($listaDezDigitosMultiplicados) % 11;
+      $condicaoRetorno = $restoDivisao11 != 0 || $restoDivisao11 != 1;
+      if ($condicaoRetorno) { return 11 - $restoDivisao11; }
+      return 0;
     }
   }
 ?>
--- php/validacao.php
+++ php/validacao.php
@@ -1,80 +1,14 @@
 <?php
+  require_once "Cpf.php";
+
   $entradaCpf = isset($_POST['entradacpf']) ? $_POST['entradacpf'] : "Erro no envio ao servidor.";
   
-  // 1. VALIDAÇÃO DA ENTRADA
-  function validandoEntrada($entCpf)
-  {
-    // 1º Tira as pontuações ".", "-"
-    $cpfRetorno = 0;
-    $cpfRetorno = preg_replace("/\.|-/", "", $entCpf);
-    // 2º Analisa se algum número está faltando
-    if (count(str_split($cpfRetorno)) != 11) {
-      $cpfRetorno = "Entrada inválida, digite conforme o texto sugerido no campo.";
-      echo json_encode([$cpfRetorno]);
-      exit(0);
-    }
-    return $cpfRetorno;
+  try {
+    $cpf = new Cpf($entradaCpf);
+  } catch (Exception $e) {
+    // Erro de formatação
+    echo json_encode(["O número recebido não é válido. Um cpf deve conter 11 dígitos numéricos."]);
   }
 
-  // 2. CALCULANDO OS ÚLTIMOS DOIS DÍGITOS DO CPF
-  function calculaUltimosDigitosCpf($entCpf, $opcao)
-  {
-    // Declaração de variáveis e escolha de J ou K
-    if ($opcao == 0) {
-      // Caso J
-      $cteOperacaoDiferenca = 10;
-      $limiteLoopMultiplicacao = 9;
-    } elseif ($opcao == 1) {
-      // Caso K
-      $cteOperacaoDiferenca = 11;
-      $limiteLoopMultiplicacao = 10;
-    }
-    $digitoRetorno = 0;
-    $cteOnze = 11;
-    $arrayCpf = str_split($entCpf);
-    $arrayCpfMultiplicado = [];
-    $somaDigitosCpf = 0;
-    $restoDivisao = 0;
-    $condicaoResto = false;
-    
-    // Multiplicação e soma dos dígitos
-    for ($i=0; $i < $limiteLoopMultiplicacao; $i++) { 
-      // Multiplicando pelas diferenças de 10.
-      $arrayCpfMultiplicado[$i] = ($cteOperacaoDiferenca - $i) * $arrayCpf[$i];
-    }
-    $somaDigitosCpf = array_sum($arrayCpfMultiplicado);
-    
-    // Analisando o resto da divisão por onze e retornando o resultado
-    $restoDivisao = $somaDigitosCpf % $cteOnze;
-    $condicaoResto = $restoDivisao == 0 || $restoDivisao == 1;
-    if ($condicaoResto) {
-      return $digitoRetorno;
-    } elseif ($restoDivisao >= 2 && $restoDivisao <= 10) {
-      return $cteOnze - $restoDivisao;
-    }
-  }
-
-  // 3. VALIDANDO OS RESULTADOS
-  function validandoResultado($entCpf, $ultimosDigitosCpf)
-  {
-    $txtResultadoValidacao = "";
-    $arrayCpf = str_split($entCpf);
-    if ($arrayCpf[count($arrayCpf)-2] == $ultimosDigitosCpf[0] && $arrayCpf[count($arrayCpf)-1] == $ultimosDigitosCpf[1]) {
-      return $resultadoValidacao = "O número de CPF digitado, $entCpf, é válido!";
-    } else {
-      return $resultadoValidacao = "O número de CPF digitado, $entCpf, não é válido!";
-    }
-  }
-
-  // EXECUÇÃO DA APLICAÇÃO
-  $valCpf = validandoEntrada($entradaCpf);
-  $arrayUltimosDigitosCpf = [];
-  for ($i=0; $i <= 1; $i++) { 
-    // Se $i for 0, então calcula-se J, se for 1, calcula-se K.
-    $arrayUltimosDigitosCpf[$i] = calculaUltimosDigitosCpf($valCpf, $i);
-  }
-
-  // 4. VALIDAÇÃO COM A ENTRADA E RETORNANDO O RESULTADO AO FRONT-END
-  $txtResultadoValidacao = [validandoResultado($valCpf, $arrayUltimosDigitosCpf)];
-  echo json_encode($txtResultadoValidacao);
+  echo json_encode([$cpf->validarCpf()]);
 ?>
